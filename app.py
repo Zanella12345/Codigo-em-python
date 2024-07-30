69b147bc2fb9337d0798ad70744e4cfdb2c6a792
Guilherme Zanella <zanella.guilherme504@gmail.com>
2024-07-30 19:36:37 -0300

    versão jogo banana 1

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 print ('ola')
 import pyautogui
-pyautogui.moveTo(1805,57, duration=2)
-pyautogui.click(1077,379)
-pyautogui.moveTo(1214,925, duration=2)
-pyautogui.move(500,0,duration=2)
+from time import sleep
+pyautogui.moveTo(1581,615, duration=2)
+for i in range(1000):
+    pyautogui.click()
