b9f29fc6010cf419a4f35accaf4343d3b405cb74
Guilherme Zanella <zanella.guilherme504@gmail.com>
2024-07-30 19:13:45 -0300

    tentative mine 1

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 print ('ola')
 import pyautogui
 pyautogui.moveTo(1805,57, duration=2)
-pyautogui.moveTo(3134,14, duration=2)
+pyautogui.click(1077,379)
+pyautogui.moveTo(1214,925, duration=2)
+pyautogui.move(500,0,duration=2)
