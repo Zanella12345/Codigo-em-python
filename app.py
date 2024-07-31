d834bd0b9fe989699bdfa25c4a9b432291ade4b1
Guilherme Zanella <zanella.guilherme504@gmail.com>
2024-07-30 21:06:59 -0300

    spam de msg no ways

--- app.py
+++ app.py
@@ -1,5 +1,10 @@
 print ('ola')
 import pyautogui
 from time import sleep
-pyautogui.moveTo(1581,615, duration=2)
-pyautogui.click(clicks=100,interval=0.06,button='left')
+for i in range(100):
+    pyautogui.moveTo(2532,418, duration=2)
+    pyautogui.dragTo(3440,419, duration=5)
+    pyautogui.moveTo(3711,959, duration=2)
+    pyautogui.leftClick(duration=2)
+    pyautogui.moveTo(2764,1051, duration=2)
+    pyautogui.leftClick(duration=2) 
