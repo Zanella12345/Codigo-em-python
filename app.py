d4ed25ec6d42d38d8e4c507fe05e3e3cffc4146d
Guilherme Zanella <zanella.guilherme504@gmail.com>
2024-07-30 21:35:09 -0300

    começando um automoção

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
-print ('ola')
 import pyautogui
 from time import sleep
-for i in range(100):
-    pyautogui.moveTo(2532,418, duration=2)
-    pyautogui.dragTo(3440,419, duration=5)
-    pyautogui.moveTo(3711,959, duration=2)
-    pyautogui.leftClick(duration=2)
-    pyautogui.moveTo(2764,1051, duration=2)
-    pyautogui.leftClick(duration=2) 
+import pyperclip
+def escrever_frase(frase):
+    pyperclip.copy(frase)
+    pyautogui.hotkey('ctrl','v')
+for i in range(20):
+    pyautogui.moveTo(2831,976, duration=0.1)
+    pyautogui.leftClick(duration=0.1)
+    escrever_frase('fala rapaziada')
+    pyautogui.moveTo(3713,984, duration=0.1)
+    pyautogui.leftClick(duration=0.1)
